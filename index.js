5b33dde843e2f620dd49bdee0cdaaec949867bc8
Bruno-TecnoB <bruno_santana@tecnobil.dev>
2025-09-03 16:07:53 -0300

    primeiro commit

---
+++ index.js
@@ -0,0 +1,22 @@
+const express = require("express");
+const app = express();
+const port = 3000;
+const path = require("path")
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.get("/api", (req, res) => {
+    res.sendFile(path.join(__dirname, "form.html"))});
+
+app.post("/api/usuarios", (req, res) => {
+    const novoUsuario = req.body;
+    res.status(201).json({
+        mensagem:"Usuário criado com sucesso!",
+        usuario: novoUsuario,
+    })
+})
+
+app.listen(port, () => {
+    console.log("Rodando na porta 3000")
+})

bc153873a5be4df218094be8415f6a020dea15c5
Bruno-TecnoB <bruno_santana@tecnobil.dev>
2025-09-04 13:34:42 +0000

    Teste Deploy Automático 2

--- webhook.js
+++ webhook.js
@@ -12,7 +12,7 @@ http.createServer((req, res) => {
         // Só executa se o push for na branch producao
         if (payload.ref === "refs/heads/producao") {
           console.log("🚀 Deploy iniciado...");
-          exec("sh /caminho/da/sua/app/deploy.sh", (err, stdout, stderr) => {
+          exec("sh /var/www/tecnobil.dev/deploy.sh", (err, stdout, stderr) => {
             if (err) console.error("Erro:", err);
             console.log(stdout);
             console.error(stderr);

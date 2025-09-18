4e2adf9a338a310e7ede1781758274950386da80
Bruno-TecnoB <bruno_santana@tecnobil.dev>
2025-09-18 11:56:33 -0300

    teste integracao

--- routes/MercadoLivreRouter.js
+++ routes/MercadoLivreRouter.js
@@ -3,8 +3,6 @@ const router = express.Router();
 const integracaoML = require("../controllers/MercadoLivreController");
 
 //Rotas para Integrações
-router.post("/mercadolivre", integracaoML.criarIntegracaoML);
-router.get("/mercadolivre/callback", integracaoML.concluirIntegracaoML);
 
 //Rota para Conexão com API
 router.post("/api/webhook", integracaoML.obterDespacho);

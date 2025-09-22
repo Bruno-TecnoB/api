ff94c308c5e0be027949b3c7224dca4c031b2c61
Bruno-TecnoB <bruno_santana@tecnobil.dev>
2025-09-22 10:36:01 -0300

    Teste integração 4

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 require("dotenv").config({ quiet: true });
-const { connectRabbitMQ, QUEUE_NAME, channel } = require("./rabbitmq");
+let { connectRabbitMQ, QUEUE_NAME, channel } = require("./rabbitmq");
 const EnviosColeta = require("./routes/ColetaRouter");
 const sequelize = require("./db/conn");
 

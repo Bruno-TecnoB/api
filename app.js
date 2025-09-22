0c5a58e81f695a7f8f4be7b57257de3e9d5147f3
Bruno-TecnoB <bruno_santana@tecnobil.dev>
2025-09-22 10:50:46 -0300

    Teste integração 4

--- app.js
+++ app.js
@@ -26,7 +26,7 @@ app.use(bodyParser.json());
 })();
 console.log("Aguardando mensagens na fila");
 // Endpoint que recebe webhook
-app.post("/webhook/ML", async (req, res) => {
+app.post("/ML", async (req, res) => {
   if (!req.body) {
     console.log("o Payload veio Vazio no APP.js");
     return res.status(200).send();

bf2287251e1eeb4b090293f8fcafb78d6ae151f2
Bruno-TecnoB <bruno_santana@tecnobil.dev>
2025-09-22 14:57:15 -0300

    Teste integração 4

--- controllers/MercadoLivreController.js
+++ controllers/MercadoLivreController.js
@@ -3,28 +3,34 @@ const fs = require("fs").promises;
 const path = require("path");
 const { connectRabbitMQ } = require("../rabbitmq");
 const { RETRY_QUEUE } = require("../rabbitmq");
-require("dotenv").config();
+require("dotenv").config({ quiet: true });
 
 const arquivoPath = path.join(__dirname, "../json/pedidos.json");
 
-// Função principal para processar payload
 async function processarPayload(req, res) {
   const body = req.body;
-  //console.log("Body_ProcessarPayload:", body);
+  console.log("Body Controller: ", body);
   const numeroPedido = body?.dados?.id;
   const plataforma = (body?.dados?.nomeEcommerce || "").trim().toLowerCase();
+  const FormaEnvio = (body?.dados?.formaEnvio.descricao || "")
+    .trim()
+    .toLowerCase();
   let order_id = body?.dados?.idPedidoEcommerce;
   const codigoSituacao = (body?.dados?.codigoSituacao || "").toLowerCase();
-
   if (!order_id) {
-    return res.status(400).json({ error: "Nenhum pedido encontrado." });
+    console.log({ error: "Nenhum pedido encontrado." });
+    return;
   }
 
   if (plataforma !== "mercado livre") {
-    return res.status(200).json({ message: "Plataforma não é Mercado Livre" });
+    // console.log({ error: `Plataforma não encontrada: ${plataforma}` });
+    return;
   }
 
-  // Mapear situação
+  if (FormaEnvio == "MercadoEnvios Flex") {
+    return;
+  }
+
   const situacoesMap = {
     aberto: "em aberto",
     aprovado: "Aguardando Separação",
@@ -49,29 +55,30 @@ async function processarPayload(req, res) {
           },
         }
       );
-
       const observacoes = tinyResp.data?.retorno?.pedido?.observacoes || "";
-      const match = observacoes.match(/\d+/);
+      const id_tiny = observacoes.match(/\d+/);
 
-      if (!match) {
+      if (!id_tiny) {
         return res.status(404).json({
           error: "Não foi possível extrair o ID do pedido ML do Tiny",
         });
       }
 
-      idPedido = match[0];
+      idPedido = id_tiny[0];
     } else {
       idPedido = order_id;
     }
 
-    // Buscar shippingId no ML
     let shippingId = null;
+    let resultadoReqML = null;
     try {
       const responseOrder = await axios.get(
         `https://api.mercadolibre.com/orders/${idPedido}`,
         { headers: { Authorization: `Bearer ${process.env.TOKEN_ML}` } }
       );
       shippingId = responseOrder.data?.shipping?.id;
+      resultadoReqML = responseOrder.data;
+      resultadoReqML.indentificador = "responseOrder ";
     } catch (err) {
       if (err.response?.status === 404) {
         const responsePack = await axios.get(
@@ -79,12 +86,13 @@ async function processarPayload(req, res) {
           { headers: { Authorization: `Bearer ${process.env.TOKEN_ML}` } }
         );
         shippingId = responsePack.data?.shipment?.id;
+        resultadoReqML = responsePack.data;
+        resultadoReqML.indentificador = "ResponsePack ";
       } else {
         throw err;
       }
     }
 
-    // Buscar status e expected_date
     let mlStatus = null;
     let expectedDate = null;
 
@@ -94,7 +102,7 @@ async function processarPayload(req, res) {
         { headers: { Authorization: `Bearer ${process.env.TOKEN_ML}` } }
       );
       mlStatus = shippingResp.data?.status;
-      //expectedDate = shippingResp.data?.expected_date;
+      expectedDate = shippingResp.data?.expected_date;
     } else {
       mlStatus = null;
       console.log("Nenhum shippingId encontrado para o pedido:", idPedido);
@@ -109,30 +117,32 @@ async function processarPayload(req, res) {
       return;
     }
 
-    // Montar pedido
+    const status = {
+      on_time: "No Prazo",
+      delayed: "Atrasado",
+    };
+
     const pedido = {
       order_id: idPedido,
       id_tiny: numeroPedido,
       plataforma: "Mercado Livre",
       status: situacaoDefinida,
-      status_ml: mlStatus,
-      expected_date: expectedDate,
-      updated_at: new Date().toISOString(),
+      status_da_api: status[mlStatus] || "Sem status",
+      data_Maxima_de_Despacho: expectedDate,
+      data_criacao: new Date().toISOString(),
     };
 
-    console.log(pedido);
-
-    // Salvar ou atualizar de forma segura
+    console.log("Pedido: ", pedido);
     await salvarOuAtualizarPedido(pedido);
-    return pedido;
+
+    return;
   } catch (err) {
     console.error(
       "Erro em processarPayload:",
       err?.response?.data || err.message || err
     );
-    return res
-      .status(500)
-      .json({ error: err?.response?.data || err.message || err });
+    console.log({ error: err?.response?.data || err.message || err });
+    return;
   }
 }
 
@@ -156,9 +166,8 @@ async function carregarPedidos() {
     const conteudo = await fs.readFile(arquivoPath, "utf-8");
     return JSON.parse(conteudo);
   } catch (err) {
-    if (err.code === "ENOENT") return []; // Arquivo não existe
+    if (err.code === "ENOENT") return [];
     if (err instanceof SyntaxError) {
-      // Renomeia arquivo corrompido para análise posterior
       const backupPath = arquivoPath + ".corrompido_" + Date.now();
       await fs.rename(arquivoPath, backupPath);
       console.error(`Arquivo JSON corrompido renomeado para: ${backupPath}`);

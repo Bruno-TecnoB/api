8253a413e6c873179a51eca2511a5546724db8e0
Bruno-TecnoB <bruno_santana@tecnobil.dev>
2025-09-22 14:39:00 -0300

    Teste integração 4

--- app.js
+++ app.js
@@ -17,10 +17,8 @@ app.use(bodyParser.json());
 
     // Inicializa servidor só depois de conectar RabbitMQ
     await sequelize.sync();
-
-    const PORT = process.env.PORT || 5000;
-    app.listen(PORT, "0.0.0.0", () => {
-      console.log(`Servidor rodando na porta ${PORT}`);
+    app.listen(process.env.PORT || 5000, () => {
+      // console.log("Servidor rodando na porta 5000");
     });
   } catch (err) {
     console.error("Erro ao inicializar RabbitMQ:", err);

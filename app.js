c9e19f246e9326053351f9c47f53b88ba590a2d6
Bruno-TecnoB <bruno_santana@tecnobil.dev>
2025-09-23 10:26:22 -0300

    Teste integração 5

--- app.js
+++ app.js
@@ -25,7 +25,7 @@ app.use(bodyParser.json());
 })();
 
 // Endpoint que recebe webhooks
-app.post("/tiny1", async (req, res) => {
+app.post("/", async (req, res) => {
   try {
     if (!channel)
       return res.status(500).send("Canal RabbitMQ não inicializado.");
